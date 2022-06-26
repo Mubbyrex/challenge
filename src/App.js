069ecf633c37568f973a7fda850d007caccf6b1e
mubaraq <mubbyre6xx@gmail.com>
2022-06-26 21:25:43 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import data from './data'
 
 function App() {
+  const [page,setPage]=useState(data)
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="App ">
+
+      {/* Heading */}
+      <section  >
+        <h1 className='text-4xl font-semibold '>
+      {page.heading}
+      </h1>
+      <h2 >
+      {page.subHeading}</h2>
+      </section>
+
+      {/* Body */}
+      <section className='flex'>
+
+        {/* write up */}
+        <div >
+        <p className='font-semibold text-grey-700'>
+          {page.paragraph}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>
+          {/* Author */}
+          <span className='flex flex-col'>
+          {page.author.image}
+          <p> rating </p>
+          {page.author.name}
+          </span>
+          completd by 21 learners
+        </div>
+      {/* buttons */}
+      <div className=''>
+        <a href='#'>See Class Schedule </a>
+        <a href='#'>Save </a>
+        <a href='#'>Share </a>
+      </div>
+      
+        </div>
+
+        {/* images */}
+        <div className='flex flex-col flex-wrap ' >
+          <img src={data.paintings[0]} alt="" />
+         
+            <img src={data.paintings[1]} alt="" srcset="" />
+            <img src={data.paintings[2]} alt="" srcset="" />
+         
+          
+        </div>
+
+      </section>
+     
+
+  
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,15 @@
+const data = {
+    heading: 'Summer Art Camp! 5 Days of Artists and Painting Monet, Van Gogh, Matisse, & More',
+    subHeading: 'Multi-Day Course',
+    paragraph: 'In this 5 day class we will explore artists Monet, Matisse, Van Gogh, among others and them recreate paintings using crayon and watercolor. Students will have fun learning about this artists & creating their own art inspired by their work',
+    author:{
+        image:'',
+        name:'Kimberly R Moseler'
+    },
+    paintings:[
+        'https://cdn.shortpixel.ai/spai/w_705+q_lossy+ret_img+to_webp/https://www.theartist.me/wp-content/uploads/2015/07/The-Starry-Night.jpg','https://www.befunky.com/images/wp/wp-2017-04-Photo-To-Oil-Painting-Header-Image.jpeg?auto=avif,webp&format=jpg&width=1150&crop=16:9','https://m.media-amazon.com/images/I/91TN+nabSWL._AC_SL1500_.jpg',
+    ],
+}
+
+
+export default data;
